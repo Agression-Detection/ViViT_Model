845959a2b0eff81b91f5e9633fa111c137abf919
BTCMauxri <mauriciov4795@gmail.com>
2026-04-28 13:13:29 -0500

    Updated: train.py

--- src/train.py
+++ src/train.py
@@ -5,10 +5,11 @@ import zipfile
 import tarfile
 import torch
 import torch.distributed as dist
+import torch.nn as nn
+import torch.optim as optim
 from transformers import VivitForVideoClassification
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader, DistributedSampler
-
 from dataset import ViolentVideoDataset
 
 # init DDP
@@ -46,8 +47,31 @@ def download_data(bucket: str, key: str, local_path: str):
         zip_ref.extractall("./data")
     print("Data extracted")
 
-def train(epochs: int):
-    pass
+def train(epochs: int, model, train_loader, train_sampler, optimizer, criterion, device, is_dist):
+    print("Training Began...")
+
+    for epoch in range(epochs):
+        if is_dist and train_sampler is not None:
+            train_sampler.set_epoch(epoch)
+        
+        model.train()
+        running_loss = 0.0
+
+        for batch_idx, (videos, labels) in enumerate(train_loader):
+            videos, labels = videos.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(videos).logits
+            loss = criterion(outputs, labels)
+
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            if batch_idx % 10 == 0:
+                print(f"Epoch [{epoch+1}/{epochs}] | Batch {batch_idx} | Loss: {loss.item():.4f}")
+        print(f"--- Epoch {epoch+1} Complete. Avg Loss: {running_loss/len(train_loader):.4f} ---")
 
 
 def validate():
@@ -61,12 +85,17 @@ def test():
 def get_dataloader(datapath: str, is_dist: bool, num_workers = 2, augment = False, batch_size = 16):
     dataset = ViolentVideoDataset(datapath)
     distributed_sampler = None
+    shuffle_data = True
+
     if is_dist:
         distributed_sampler = DistributedSampler(dataset, shuffle=True)
+        shuffle_data = False
+
     # TODO: Data must be pulled at random from dataset
     dataloader =  DataLoader(
         dataset,
         batch_size = batch_size,
+        shuffle = shuffle_data,
         sampler = distributed_sampler,
         num_workers = num_workers
     )
@@ -91,6 +120,8 @@ if __name__ == '__main__':
     os.makedirs(args.checkpoint_dir, exist_ok=True)
     device = get_device(local_rank, is_dist)
 
+    model = get_model(device, is_dist, local_rank)
+
     train_data_path = os.path.join(args.data_dir, 'train')
     test_data_path = os.path.join(args.data_dir, 'test')
     valid_data_path = os.path.join(args.data_dir, 'val')
@@ -99,9 +130,11 @@ if __name__ == '__main__':
     file_name = 'data/videos'
     #download_data(bucket, file_name, f"{args.data_dir}/videos")
 
-    train_loader = get_dataloader(train_data_path, is_dist)
+    train_loader, train_sampler = get_dataloader(train_data_path, is_dist, batch_size=args.batch_size)
     # valid_loader = get_dataloader(args.data_dir)
     # test_loader = get_dataloader(args.data_dir)
 
+    optimizer = optim.AdamW(model.parameters(), lr=1e-4)
+    criterion = nn.CrossEntropyLoss()
 
-
+    train(args.epochs, model, train_loader, train_sampler, optimizer, criterion, device, is_dist)
