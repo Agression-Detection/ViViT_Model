b5153f4e26fc06229ef8ca2cbcb79646e998b730
BTCMauxri <mauriciov4795@gmail.com>
2026-04-28 09:03:12 -0500

    Fixed: dataset and getModel

--- src/dataset.py
+++ src/dataset.py
@@ -5,17 +5,23 @@ from torch.utils.data import Dataset
 import os
 
 class ViolentVideoDataset(Dataset):
-    def __init__(self, tensor_dir):
-        self.tensor_paths = []
-        self.labels = []
+    def __init__(self, data_dir):
+        self.tensor_paths: list[str] = []   # file names Same index
+        self.labels: list[int] = []         # values in labels
 
-        for label_name, label_val in [("Safe", 0), ("Violent", 1)]:
-            folder_path = os.path.join(tensor_dir, label_name)
-            if os.path.exists(folder_path):
-                for file in os.listdir(folder_path):
-                    if file.endswith('.pt'):
-                        self.tensor_paths.append(os.path.join(folder_path, file))
-                        self.labels.append(label_val)
+        video_dir = os.path.join(data_dir, 'video')
+        labels_dir = os.path.join(data_dir, 'labels')
+
+        for each in os.listdir(video_dir):
+            if each.endswith('.pt'):
+                self.tensor_paths.append(os.path.join(video_dir, each))
+                label_file = each.replace('.pt', '.txt')
+                label_path = os.path.join(labels_dir, label_file)
+                with open(label_path, 'r') as f:
+                    self.labels.append(int(f.read().strip()))
+
+        print(self.tensor_paths[:5])
+        print(self.labels[:5])
 
     def __len__(self):
         return len(self.tensor_paths)
--- src/train.py
+++ src/train.py
@@ -4,10 +4,39 @@ import boto3
 import zipfile
 import tarfile
 import torch
+import torch.distributed as dist
+from transformers import VivitForVideoClassification
+from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader, DistributedSampler
 
 from dataset import ViolentVideoDataset
 
+# init DDP
+def init_ddp():
+    if "RANK" in os.environ:
+        dist.init_process_group(backend='nccl')
+        local_rank = int(os.environ["LOCAL_RANK"])
+        torch.cuda.set_device(local_rank)
+        return True, local_rank
+    return False, None
+
+
+def get_device(local_rank, use_ddp):
+    if torch.cuda.is_available():
+        return torch.device(f"cuda:{local_rank}" if use_ddp else "cuda:0")
+    return torch.device("cpu")
+ 
+
+
+def get_model(device, is_dist, local_rank):
+    model = VivitForVideoClassification.from_pretrained(
+        "google/vivit-b-16x2-kinetics400", 
+        num_labels=2, 
+        ignore_mismatched_sizes=True
+    )
+    model = model.to(device)
+    if is_dist: model = DDP(model, device_ids=[local_rank])
+    return model
 
 def download_data(bucket: str, key: str, local_path: str):
     s3 = boto3.client('s3')
@@ -29,12 +58,19 @@ def test():
     pass
 
 
-def get_dataloader(datapath: str):
+def get_dataloader(datapath: str, is_dist: bool, num_workers = 2, augment = False, batch_size = 16):
     dataset = ViolentVideoDataset(datapath)
     distributed_sampler = None
+    if is_dist:
+        distributed_sampler = DistributedSampler(dataset, shuffle=True)
     # TODO: Data must be pulled at random from dataset
-    dataloader = None
-    return
+    dataloader =  DataLoader(
+        dataset,
+        batch_size = batch_size,
+        sampler = distributed_sampler,
+        num_workers = num_workers
+    )
+    return dataloader, distributed_sampler
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -48,10 +84,12 @@ def parse_args():
 
 
 if __name__ == '__main__':
+    is_dist, local_rank = init_ddp()
     args = parse_args()
     os.makedirs(args.model_dir, exist_ok=True)
     os.makedirs(args.data_dir, exist_ok=True)
     os.makedirs(args.checkpoint_dir, exist_ok=True)
+    device = get_device(local_rank, is_dist)
 
     train_data_path = os.path.join(args.data_dir, 'train')
     test_data_path = os.path.join(args.data_dir, 'test')
@@ -59,9 +97,9 @@ if __name__ == '__main__':
 
     bucket = 'agression-model'
     file_name = 'data/videos'
-    download_data(bucket, file_name, f"{args.data_dir}/videos")
+    #download_data(bucket, file_name, f"{args.data_dir}/videos")
 
-    # train_loader = get_dataloader(args.data_dir)
+    train_loader = get_dataloader(train_data_path, is_dist)
     # valid_loader = get_dataloader(args.data_dir)
     # test_loader = get_dataloader(args.data_dir)
 
